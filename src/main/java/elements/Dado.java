cdabb043c5c93f4103a255bb8790da159d0f5121
Ruben <ruben.mte@gmail.com>
2023-02-22 20:37:32 +0100

    Creación del objeto Dado

--- src/main/java/elements/Dado.java
+++ src/main/java/elements/Dado.java
@@ -1,4 +1,16 @@
 package elements;
 
+import java.util.List;
+
 public class Dado {
+
+    public Dado(){
+
+    }
+
+    public int lanzar(){
+        int resultado = (int)(Math.random()*6+1);
+        return resultado;
+    }
+
 }
