bc7a488366f977367ca863499a792a0266b75745
Ruben <ruben.mte@gmail.com>
2023-02-23 20:44:01 +0100

    Creación de la baraja española

--- src/main/java/elements/SpanishDeck.java
+++ src/main/java/elements/SpanishDeck.java
@@ -2,15 +2,33 @@ package elements;
 
 public class SpanishDeck {
 
-    public SpanishDeck(){
-
-    }
-
-    String values [] = {"1", "2", "3", "4", "5", "6", "7", "sota", "caballo", "rey"};
-    String suits [] = {"oros", "copas", "espadas", "bastos"};
+    String[] values = {"1", "2", "3", "4", "5", "6", "7", "sota", "caballo", "rey"};
+    String[] suits = {"oros", "copas", "espadas", "bastos"};
 
     String[] deck = new String[40];
 
+    public SpanishDeck(){
+        createDeck();
+    }
 
+
+
+    public void createDeck(){
+        int index = 0;
+
+        for(int i = 0; i<suits.length; i++){
+            for(int j=0; j<values.length; j++){
+                String card = values[j]+" de "+suits[i];
+                deck[index] = card;
+                index++;
+            }
+        }
+    }
+
+    public String randomCard() {
+        int position = (int)(Math.random()*40);
+        String card = deck[position];
+        return card;
+    }
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import elements.SpanishDeck;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Bienvenido a la app de casino");
 
+        SpanishDeck spanishDeck = new SpanishDeck();
+
+        for (int i=0; i<200; i++) {
+            System.out.println(spanishDeck.randomCard());
+        }
     }
 }
