90b9c371c0ff95090402be105a302fc64fc97a9d
Ruben <ruben.mte@gmail.com>
2023-03-17 22:34:05 +0100

    Modificación de textos

--- src/main/java/elements/Texts.java
+++ src/main/java/elements/Texts.java
@@ -3,9 +3,14 @@ package elements;
 public class Texts {
 
     public String wellcome = "Bienvenido a la app de casino. ¿A qué desea jugar?";
-    public String availableGames = "1. BlackJack.\n2. Carta más alta.";
+    public String availableGames =
+            "0. Salir" +
+            "\n1. BlackJack." +
+            "\n2. Carta más alta.";
 
     public String notImplemented = "Todavía no se puede. Pronto llegará.";
+    public String thanks = "Gracias por jugar. Vuelva pronto.";
+    public String notAvailable = "Opción no disponible. Seleccione otra.";
 
     public String blackJackGameContinue = "\n¿Que desea hacer a continuacion?\n1. Separar cartas\n2. Pedir nueva carta\n3. Plantarse";
 
