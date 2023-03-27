c30a05928616e68675e414f10c6c7b1e6718641b
Ruben <ruben.mte@gmail.com>
2023-03-27 18:22:17 +0200

    Modificacion del juego Ruleta

--- src/main/java/games/Roulette.java
+++ src/main/java/games/Roulette.java
@@ -1,35 +1,41 @@
 package games;
 
 import elements.Pocket;
+import elements.Texts;
 
 import java.util.Random;
 import java.util.Scanner;
 
 public class Roulette {
 
-    Pocket pocket;
+    Scanner scanner = new Scanner(System.in);
+    Pocket pocket = new Pocket();
+    Texts texts = new Texts();
+    int userNumber = 37;
 
     public Roulette() {
-        // Inicializar la ruleta
-        Random r = new Random();
-        int numeroGanador = r.nextInt(37);
+        System.out.println(texts.rouletteWellcome);
 
-        // Pedir al usuario que ingrese un número
-        Scanner input = new Scanner(System.in);
-        System.out.print("Ingresa un número entre 0 y 36: ");
-        int numeroUsuario = input.nextInt();
+        System.out.println(texts.rouletteBet);
 
-        // Mostrar el número ganador
-        System.out.println("El número ganador es: " + numeroGanador);
 
-        // Determinar si el usuario ha ganado
-        if (numeroUsuario == numeroGanador) {
+        System.out.print(texts.rouletteChooseNumber);
+        String selection = scanner.nextLine();
+        try{
+            userNumber = Integer.parseInt(selection);
+        } catch (Exception e) {
+            System.out.println(selection + " no es un número válido");
+        }
+        
+        int winnerNumber = pocket.newLaunch();
+        System.out.println("El número ganador es: " + winnerNumber);
+        
+        if (userNumber == winnerNumber) {
             System.out.println("Felicidades, has ganado!");
         } else {
             System.out.println("Lo siento, no has ganado.");
         }
 
-        pocket = new Pocket();
     }
 
 }
