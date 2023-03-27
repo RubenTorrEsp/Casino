ae79163003f8c93b171c9cd7c6476440626f42b9
Ruben <ruben.mte@gmail.com>
2023-03-27 18:21:54 +0200

    Creacion de la clase Pocket

--- src/main/java/elements/Pocket.java
+++ src/main/java/elements/Pocket.java
@@ -1,17 +1,39 @@
 package elements;
 
-public class Pocket {
-    int number;
-    Boolean isBlack;
-    Boolean isPair;
-    Boolean isPass;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-    public Pocket(){
-        newLaunch();
+public class Pocket {
+
+    List<Integer> blackNumbers = new ArrayList<>(Arrays.asList(
+            2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35
+    ));
+
+    public Pocket(){}
+
+    public int newLaunch(){
+        Random r = new Random();
+        int winner = r.nextInt(37);
+        return winner;
     }
 
-    public void newLaunch(){
-        number = (int)(Math.random()*36);
-        System.out.println("No va mas...... "+number);
+    public Boolean isBlack(int number){
+        Boolean isBlack = false;
+        if (blackNumbers.contains(number)) isBlack = true;
+        return isBlack;
+    }
+
+    public Boolean isPair(int number){
+        Boolean isPair = false;
+        if (number % 2 == 0) isPair = true;
+        return isPair;
+    }
+
+    public Boolean isPass(int number){
+        Boolean isPass = false;
+        if (number >= 18) isPass = true;
+        return isPass;
     }
 }
