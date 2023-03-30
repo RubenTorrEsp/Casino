b5f8fde75df2cce6b443a8b9e6e454c730d49cdd
Ruben <ruben.mte@gmail.com>
2023-03-30 20:31:03 +0200

    Modificacion de los texts

--- src/main/java/elements/Texts.java
+++ src/main/java/elements/Texts.java
@@ -9,23 +9,19 @@ public class Texts {
             "\n1. BlackJack." +
             "\n2. Carta alta." +
             "\n3. Ruleta.";
-
     public String quantityToPlay = "¿Cuanto quieres apostar?";
     public String notImplemented = "Todavía no se puede. Pronto llegará.";
     public String thanks = "Gracias por jugar. Vuelva pronto.";
     public String notAvailable = "Opción no disponible. Seleccione otra.";
-    public String correctBet(int quantity) {return "Genial! Ahora tienes "+quantity+" euros en fichas. Gástalos bien.";}
-    public String crupierValue(int quantity) { return "El crupier ha sacado "+ quantity +" puntos."; }
+    public String correctBet(int quantity) { return "Genial! Ahora tienes "+quantity+" euros en fichas. Gástalos bien.";}
+    public String crupierValue(int quantity) { return "El crupier ha sacado "+ quantity +" puntos.";}
+    public String notValidValue(String quantity) { return quantity +" no es un número válido";}
 
     //BlackJack texts
     public String blackJackWellcome = "Bienvenido a la mesa de BlackJack";
     public String blackJackGameContinue = "\n¿Que desea hacer a continuacion?\n1. Separar cartas\n2. Pedir nueva carta\n3. Plantarse";
-    public String stucked(int quantity){
-        return "Te has plantado con " + quantity + " puntos. A ver si hay suerte.";
-    }
-    public String totalValue(int quantity){
-        return "El valor total de sus cartas es " + quantity + ".";
-    }
+    public String stucked(int quantity){ return "Te has plantado con " + quantity + " puntos. A ver si hay suerte.";}
+    public String totalValue(int quantity){ return "El valor total de sus cartas es " + quantity + ".";}
 
     //High Card texts
     public String highCardWellcome = "Bienvenido a la mesa de Carta Alta. Ganará el jugador que saque la carta más alta.";
