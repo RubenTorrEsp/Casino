e0d4f8bf3c94c74b0ffb5f5a9639148f1eb3f028
Ruben <ruben.mte@gmail.com>
2023-04-11 20:03:08 +0200

    Creacion de la clase User

--- src/main/java/utils/User.java
+++ src/main/java/utils/User.java
@@ -1,4 +1,32 @@
 package utils;
 
 public class User {
+
+    int id;
+    String userName;
+    int money;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
 }
