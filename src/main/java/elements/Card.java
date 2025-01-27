6418f2e5334bfcbe441b6efcaadae9c02a4232b7
Ruben <ruben.mte@gmail.com>
2025-01-27 16:58:12 +0100

    Modificacion de sentencia switch-case con if

--- src/main/java/elements/Card.java
+++ src/main/java/elements/Card.java
@@ -27,50 +27,32 @@ public class Card {
 
     public int getValue() {
         int value = 0;
-        switch (rank){
-            case "A":
-            case "1":
-                value = 1;
-                break;
-            case "2":
-                value = 2;
-                break;
-            case "3":
-                value = 3;
-                break;
-            case "4":
-                value = 4;
-                break;
-            case "5":
-                value = 5;
-                break;
-            case "6":
-                value = 6;
-                break;
-            case "7":
-                value = 7;
-                break;
-            case "8":
-                value = 8;
-                break;
-            case "9":
-                value = 9;
-                break;
-            case "10":
-            case "sota":
-                value = 10;
-                break;
-            case "J":
-            case "caballo":
-                value = 11;
-                break;
-            case "Q":
-            case "rey":
-                value = 12;
-                break;
-            case "K":
-                value = 13;
-                break;
+        if (rank == "A" || rank == "1") {
+            value = 1;
+        } else if (rank == "2") {
+            value = 2;
+        } else if (rank == "3") {
+            value = 3;
+        } else if (rank == "4") {
+            value = 4;
+        } else if (rank == "5") {
+            value = 5;
+        } else if (rank == "6") {
+            value = 6;
+        } else if (rank == "7") {
+            value = 7;
+        } else if (rank == "8") {
+            value = 8;
+        } else if (rank == "9") {
+            value = 9;
+        } else if (rank == "10" || rank == "sota") {
+            value = 10;
+        } else if (rank == "J" || rank == "caballo") {
+            value = 11;
+        } else if (rank == "Q" || rank == "rey") {
+            value = 12;
+        } else if (rank == "K") {
+            value = 13;
         }
         return value;
     }
