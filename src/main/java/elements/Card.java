9d9b2ce86ffb2291a703a4126065117edc529513
Ruben <ruben.mte@gmail.com>
2025-01-28 08:27:50 +0100

    Modificacion de sentencia switch-case con if

--- src/main/java/elements/Card.java
+++ src/main/java/elements/Card.java
@@ -59,44 +59,26 @@ public class Card {
 
     public int getValueCourtCards(){
         int value = 0;
-        switch (rank){
-            case "A":
-            case "1":
-                value = 1;
-                break;
-            case "2":
-                value = 2;
-                break;
-            case "3":
-                value = 3;
-                break;
-            case "4":
-                value = 4;
-                break;
-            case "5":
-                value = 5;
-                break;
-            case "6":
-                value = 6;
-                break;
-            case "7":
-                value = 7;
-                break;
-            case "8":
-                value = 8;
-                break;
-            case "9":
-                value = 9;
-                break;
-            case "10":
-            case "J":
-            case "Q":
-            case "K":
-            case "sota":
-            case "caballo":
-            case "rey":
-                value = 10;
-                break;
+        if (rank == "A" || rank == "1") {
+            value = 1;
+        } else if (rank == "2") {
+            value = 2;
+        } else if (rank == "3") {
+            value = 3;
+        } else if (rank == "4") {
+            value = 4;
+        } else if (rank == "5") {
+            value = 5;
+        } else if (rank == "6") {
+            value = 6;
+        } else if (rank == "7") {
+            value = 7;
+        } else if (rank == "8") {
+            value = 8;
+        } else if (rank == "9") {
+            value = 9;
+        } else if (rank == "10" || rank == "J" || rank == "Q" || rank == "K" || rank == "sota" || rank == "caballo" || rank == "rey") {
+            value = 10;
         }
         return value;
     }
