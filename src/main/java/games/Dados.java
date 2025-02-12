b29fdbb4d09231de11d1cb8fbe36533517cdef63
Ruben <ruben.mte@gmail.com>
2025-02-12 12:10:54 +0100

    Creacion del user en la clase Dados

--- src/main/java/games/Dados.java
+++ src/main/java/games/Dados.java
@@ -4,12 +4,14 @@ import utils.User;
 
 public class Dados {
 
+    User user;
+
     public Dados() {
         System.out.println("En proceso de creación");
     }
 
     public Dados(User user) {
-
+        this.user = user;
     }
 
 }
