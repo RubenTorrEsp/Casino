4fe56cda70fe37fd06dfe0343bba0187fa63d9f6
Ruben <ruben.mte@gmail.com>
2025-02-25 08:40:46 +0100

    Cambio de los textos con salto de linea

--- src/main/java/utils/Texts.java
+++ src/main/java/utils/Texts.java
@@ -22,7 +22,12 @@ public class Texts {
 
     //BlackJack texts
     public String blackJackWellcome = "Bienvenido a la mesa de BlackJack";
-    public String blackJackGameContinue = "\n¿Que desea hacer a continuacion?\n1. Separar cartas\n2. Pedir nueva carta\n3. Plantarse";
+    public String blackJackGameContinue =
+        """
+            ¿Que desea hacer a continuacion?\
+            1. Separar cartas\
+            2. Pedir nueva carta\
+            3. Plantarse""";
     public String stucked(int quantity){ return "Te has plantado con " + quantity + " puntos. A ver si hay suerte.";}
     public String totalValue(int quantity){ return "El valor total de sus cartas es " + quantity + ".";}
 
