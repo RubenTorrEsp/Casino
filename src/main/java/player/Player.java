6a1f80b67ef682699b43b4d81f356ba31f2a818a
Ruben <ruben.mte@gmail.com>
2025-04-15 13:34:35 +0200

    Añadido lombok Builder a la clase Player

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,14 +1,13 @@
 package player;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Player {
 
     public String name;
     public int saldo;
 
-    public Player() {
-
-    }
 }
